61021cdc22b94eb7245e278cdddf28cda4216294
Vojtech Burian <vojtech@salsitasoft.com>
2014-07-02 16:36:07 +0200

    Page object generator script

--- README.md
+++ README.md
@@ -1,4 +1,23 @@
-pagenetor
-=========
+Pagenetor - selenium webdriver page object generator
+============
 
-Script generating selenium webdriver page-object files
+Script generating page-object files from JSON data object.
+
+### How to use
+
+1. create project folder under projects
+1. create "input_data" and  "templates" folders in project folder
+1. add json file(s) with locator data (see example file)
+1. add template file to template folder, make sure the file format is: "[name_of_template].[file_extension].[tmpl]" (see example template files)
+1. set correct values in "config.properties" configuration files
+1. run "pagenerator.py"
+1. verify page object files have been created under project folder > "output" folder
+
+### Required libraries
+
+* ```pip install Jinja2```
+
+### Future enhancements
+
+* enhance existing templates and JSON files to support generation of more complex structures
+* connect with some tool that would automatically parse locator information from pages generating output JSON file (which this script could use as input value)
---
+++ pagenetor.py
@@ -0,0 +1,71 @@
+# /usr/bin/env python
+# -*- coding: utf-8 -*-
+import ConfigParser
+import os
+import glob
+import json
+
+from jinja2 import Environment, FileSystemLoader
+
+
+class PageGenerator():
+    def __init__(self):
+        self.test_vars = self.load_configuration()
+        self.project_root = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'projects',
+                                         self.test_vars.get('project_dir'))
+        self.data_folder = os.path.join(self.project_root, 'input_data')
+        self.template_folder = os.path.join(self.project_root, 'templates')
+        self.output_folder = os.path.join(self.project_root, 'output')
+        self.template_file_name = self.test_vars.get('template_file_name')
+        self.data_expression = self.test_vars.get('data_expression')
+
+    def load_configuration(self):
+        """ loads configuration variables from .properties file """
+        config = ConfigParser.ConfigParser()
+        config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'config.properties')
+        config.read(config_file)
+        return dict(config.defaults())
+
+    def run(self):
+        """ runs page object generation """
+        if not os.path.exists(self.output_folder):
+            os.makedirs(self.output_folder)
+        for entry in glob.glob(os.path.join(self.data_folder, self.data_expression)):
+            f = open(entry)
+            text = json.loads(f.read())
+            f.close()
+            self.create_page_objects(text)
+
+    def create_page_objects(self, data):
+        """ initiates page object generation for each entry in json array """
+        for page in data['pages']:
+            self.create_page(page)
+
+    def create_page(self, data):
+        """ generates one page object file """
+        env = Environment(loader=FileSystemLoader(self.template_folder), trim_blocks=True, lstrip_blocks=True)
+        template = env.get_template(self.template_file_name)
+        template_vars = {'class_name': self.get_class_name(data['name']), 'page': data}
+        output = template.render(template_vars)
+        formatted_output = output.encode('utf8').strip()
+        file_name = data['name'] + self.get_output_file_type()
+        result_html = open(os.path.join(self.output_folder, file_name), 'w')
+        result_html.write(formatted_output)
+        result_html.close()
+
+    def get_output_file_type(self):
+        """ returns file type of output page object files """
+        file_name = '.' + self.template_file_name.split('.')[-2]
+        return file_name
+
+    def get_class_name(self, name):
+        """ converts file name to class name (camel-case) """
+        name_list = name.split('_')
+        file_name = ''
+        for item in name_list:
+            file_name += item.capitalize()
+        return file_name
+
+
+generator = PageGenerator()
+generator.run()
